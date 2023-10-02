c5597d397ae2b048d78ca1f10a306d3880e08073
bakka22 <abobakereltom@gmail.com>
2023-10-02 22:36:03 +0000

    s

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+*check_cycle - find a loop in list
+*@list : head of the list
+*Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *pos1, *pos2;
+
+	if (list == NULL)
+		return (0);
+	pos1 = list;
+	pos2 = list;
+	while (pos1 && pos2 && pos2->next)
+	{
+		if (pos2 == pos1)
+			return (1);
+		pos2 = pos2->next->next;
+		pos1 = pos1->next;
+	}
+	return (0);
+}
