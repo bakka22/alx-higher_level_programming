b23eced823845b91f9058cfdbdf8af5dea0cb863
bakka22 <abobakereltom@gmail.com>
2023-10-05 21:01:36 +0000

    s

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    y = dir(sys)
+    y = dir(hidden_4)
     x = len(y)
     for i in range(x):
-        print(y[i])
+        if y[i][1] != '_':
+            print(y[i])
