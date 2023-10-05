f635876611c8c2e470e486c76d8264cf1128d60d
bakka22 <abobakereltom@gmail.com>
2023-10-05 20:49:06 +0000

    d

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    y = dir(sys)
+    x = len(y)
+    for i in range(x):
+        print(y[i])
---
+++ 0x02-python-import_modules/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+y = dir(sys)
+x = len(y)
+for i in range(x):
+	print(y[i])
