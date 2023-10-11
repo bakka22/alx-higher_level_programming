ea4abe2c83a22b4d40a3d11524445beb7653e35c
bakka22 <abobakereltom@gmail.com>
2023-10-11 22:32:49 +0000

    s

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    ls = list(a_dictionary.keys())
+    ls.sort()
+    for i in ls:
+    print("{}: {}".format(i, a_dictionary.get(i)))
