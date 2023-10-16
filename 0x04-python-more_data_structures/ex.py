0e3c3beea632e8c53a90c84521b4a32dcbe9f5a6
bakka22 <abobakereltom@gmail.com>
2023-10-16 02:15:37 +0000

    s

--- 0x04-python-more_data_structures/ex.py
+++ 0x04-python-more_data_structures/ex.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    key_list = list(a_dictionary)
-    new = {}
-    for i in key_list:
-        new[i] = a_dictionary[i]
-    return (new)
+def roman_to_int(roman_string):
+	roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D':500, 'M': 1000}
+	sum = 0
+	for i in range(len(roman_string)):
+		if i != len(roman_string) - 1:
+			if roman[roman_string[i]] == 1000:
+				sum += 1000
+			elif roman[roman_string[i]] < roman[roman_string[i + 1]]:
+				sum -= roman[roman_string[i]]
+			else:
+				sum += roman[roman_string[i]]
+		else:
+			sum += roman[roman_string[i]]
+	return sum
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
