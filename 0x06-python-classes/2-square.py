d84e4f1c01974143ad264a35628243f48fe15033
bakka22 <abobakereltom@gmail.com>
2023-10-25 02:05:45 +0000

    s

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ define a square """
+    def __init__(self, sz=0):
+        """ initialize class with args.
+
+        args:
+            self: the opj itself
+            sz: size
+
+        Returns:
+            nothing
+
+        """
+        if not isinstance(sz, int):
+            raise TypeError("size must be an integer")
+        if sz < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = sz
