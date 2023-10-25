4a66103773eda41447e2998a70610f7d42a009d7
bakka22 <abobakereltom@gmail.com>
2023-10-25 01:54:03 +0000

    s

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ define a square """
+    def __init__(self, sz=None):
+        """ initialize class with args.
+
+        args:
+            self: the opj itself
+            sz: size
+
+        Returns:
+            nothing
+
+        """
+        self.__size = sz
