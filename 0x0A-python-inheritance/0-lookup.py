b3fb259e661a1195590fde671919656510ad6e00
bakka22 <abobakereltom@gmail.com>
2023-11-06 20:00:06 +0000

    s

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+""" look up module """
+
+
+def lookup(obj):
+    """ lookup function
+    args:
+        obj: object to look at
+    Returns:
+        list of available attributes and methods of an object
+    """
+    return dir(obj)
