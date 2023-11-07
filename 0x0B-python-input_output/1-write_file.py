fb810da229ac1ada6d1fdfc866c773dedee2eba2
bakka22 <abobakereltom@gmail.com>
2023-11-07 17:14:10 +0000

    s

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" write_file function """
+
+
+def write_file(filename="", text=""):
+    """ read_file
+    args:
+        filename: name of the file
+        text: ...
+    Returns:
+        nothing
+    """
+    with open(filename, encoding='utf-8') as w:
+        return w.write(text)
