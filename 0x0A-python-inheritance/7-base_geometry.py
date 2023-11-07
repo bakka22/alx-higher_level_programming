53d83da9b806c85c51dafb27f2ca32e704372939
bakka22 <abobakereltom@gmail.com>
2023-11-07 13:55:00 +0000

    s

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 """ geometry module """
 
+def is_same_class(obj, a_class):
+    """ is_same_class check if object is instance of a class
+    args:
+        obj: the object
+        a_class: the class
+    Returns:
+        True if the obj is instance False otherwize
+    """
+
+    x = type(obj)
+    x = str(x)
+    x = x[8:-2]
+    if x == a_class.__name__:
+        return True
+    return False
 
 class BaseGeometry:
     """ base_geometry """
@@ -23,7 +38,7 @@ class BaseGeometry:
         Returns:
             nothing
         """
-        if not isinstance(value, int):
+        if not is_same_class(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/7-base_geometry.py~
+++ 0x0A-python-inheritance/7-base_geometry.py~
@@ -5,22 +5,6 @@
 class BaseGeometry:
     """ base_geometry """
 
-def is_same_class(obj, a_class):
-    """ is_same_class check if object is instance of a class
-    args:
-        obj: the object
-        a_class: the class
-    Returns:
-        True if the obj is instance False otherwize
-    """
-
-    x = type(obj)
-    x = str(x)
-    x = x[8:-2]
-    if x == a_class.__name__:
-        return True
-    return False
-
     def area(self):
         """ area
         args:
