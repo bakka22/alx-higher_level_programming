1c1782e422ed92e66c3cf691969941accbfac6b5
bakka22 <abobakereltom@gmail.com>
2023-11-07 17:44:29 +0000

    s

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """ return json string representaion """
     with open(filename, "r", encoding="utf-8") as w:
-        json.load(w)
+        return json.load(w)
