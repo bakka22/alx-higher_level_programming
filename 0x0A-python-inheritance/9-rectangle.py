4a5e4e80bb5a0bde2e95f93da4efd4788ed84351
bakka22 <abobakereltom@gmail.com>
2023-11-07 16:28:53 +0000

    s

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" geometry module """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle """
+
+    def __init__(self, width, height):
+        """special method
+        args:
+            self: ...
+            width: ...
+            height: ...
+        Returns:
+            nothing
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ area
+        args:
+            self: ...
+        Returns:
+            area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """special method
+        args:
+            self: ...
+        Returns:
+            ...
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
