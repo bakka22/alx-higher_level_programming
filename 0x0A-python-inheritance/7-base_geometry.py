604db5413120a87b536f88b52eaff1de53d3bd07
bakka22 <abobakereltom@gmail.com>
2023-11-07 13:57:46 +0000

    s

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
 """ geometry module """
 
-def is_same_class(obj, a_class):
-    """ is_same_class check if object is instance of a class
-    args:
-        obj: the object
-        a_class: the class
-    Returns:
-        True if the obj is instance False otherwize
-    """
-
-    x = type(obj)
-    x = str(x)
-    x = x[8:-2]
-    if x == a_class.__name__:
-        return True
-    return False
 
 class BaseGeometry:
     """ base_geometry """
@@ -38,7 +23,7 @@ class BaseGeometry:
         Returns:
             nothing
         """
-        if not is_same_class(value, int):
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
