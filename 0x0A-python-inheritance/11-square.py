234dd71ae9376a105a1c36b18428b5b86730704c
bakka22 <abobakereltom@gmail.com>
2023-11-07 16:46:55 +0000

    s

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" geometry module """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Rectangle """
+
+    def __init__(self, size):
+        """special method
+        args:
+            self: ...
+            size: ...
+        Returns:
+            nothing
+        """
+        Rectangle.__init__(self, size, size)
+        self.__size == size
+
+    def __str__(slef):
+        """ special method
+        args:
+            self: ...
+        Returns:
+            ...
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
