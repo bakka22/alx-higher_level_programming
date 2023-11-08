2107bab7f23fb7ddc62b9903b92e8b70fc5fbdf5
bakka22 <abobakereltom@gmail.com>
2023-11-08 02:21:28 +0000

    s

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" student class """
+
+
+class Student:
+    """ student class """
+    def __init__(self, first_name, last_name, age):
+        """ initialize the class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ json reprisentation """
+        return self.__dict__
