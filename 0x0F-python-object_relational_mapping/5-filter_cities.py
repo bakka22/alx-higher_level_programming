db61712b37803093d5854ecc2beda1486e77ad75
bakka22 <abobakereltom@gmail.com>
2024-02-17 10:13:34 +0000

    S

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" select state module """
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], host="localhost", port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT cities.name from cities join\
+                 states on cities.state_id=states.id where states.name like\
+                 binary %s", (sys.argv[4], ))
+    rows = cur.fetchall()
+    for i in range(len(rows)):
+        if i == 0:
+            print(f"{rows[i][0]}", end="")
+        else:
+            print(f", {rows[i][0]}", end="")
+    print()
