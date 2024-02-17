460a6a1128b4c9138f4f63472d6794a985e2fd1a
bakka22 <abobakereltom@gmail.com>
2024-02-17 06:12:46 +0000

    s"

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,8 +2,7 @@
 """ select state module """
 import MySQLdb
 import sys
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], host="localhost", port=3306)
     cur = db.cursor()
