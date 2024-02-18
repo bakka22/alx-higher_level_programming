be60555e5331dbe127ec580cc6334022ae7e75fc
bakka22 <abobakereltom@gmail.com>
2024-02-18 14:12:21 +0000

    s

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    row = session.query(State).filter_by(id=2).update({'name':'New Mexico'})
+    row = session.query(State).filter_by(id=2).update({'name': 'New Mexico'})
     session.commit()
