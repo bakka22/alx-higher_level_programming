67e8e16cd413ef63c3158fc931bad7b82f2b7416
bakka22 <abobakereltom@gmail.com>
2024-03-07 22:53:58 +0000

    s

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #displays all HTTP methods the server will accept
-curl -s -X OPTIONS -I $1 | awk '/^Allow:' | cut -d' ' -f2-
+curl -s -X OPTIONS -I $1 | awk '/^Allow:/' | cut -d' ' -f2-
