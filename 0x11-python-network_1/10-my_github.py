6ed00b6e6c938dbd005c87f5bea29b30390f2326
bakka22 <abobakereltom@gmail.com>
2024-03-09 14:55:18 +0000

    s

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status using requests """
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = 'https://api.github.com/users/' + sys.argv[1]
+    heads = {'Accept': 'application/vnd.github+json',
+             'Authorization': 'Bearer ' + sys.argv[2],
+             'X-GitHub-Api-Version': '2022-11-28'
+            }
+    r = requests.get(url, headers=heads)
+    id = r.json().get('id')
+    print(id)
