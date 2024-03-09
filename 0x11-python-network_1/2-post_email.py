5811bb5d18b24b136c905e2a971c3ab859b85d5a
bakka22 <abobakereltom@gmail.com>
2024-03-09 02:40:12 +0000

    s

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
+import urllib.parse
 import sys
 if __name__ == '__main__':
     values = {'email': sys.argv[2]}
-    data = urllib.request.urlencode(values)
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
     url = sys.argv[1]
     with urllib.request.urlopen(url, data) as r:
         va = r.read().decode('utf-8')
