128d2c0e2bae5f68c6fd1021d90be2806831c029
bakka22 <abobakereltom@gmail.com>
2024-03-09 04:23:12 +0000

    s

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
             print(f"[{id}] {name}")
         else:
             print("No result")
-    except requests.exceptions.JSONDecodeError:
+    except ValueError:
         print("Not a valid JSON")
