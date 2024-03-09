fc78ba3d598ab4343c5c07e83cd03c1ed238f48b
bakka22 <abobakereltom@gmail.com>
2024-03-09 03:39:48 +0000

    s

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status using requests """
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
