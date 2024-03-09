7d2f898f18b186a274692fc2b3af50e842e8ce98
bakka22 <abobakereltom@gmail.com>
2024-03-09 03:23:03 +0000

    s

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,6 +3,6 @@
 if __name__ == '__main__':
     import requests
     r = requests.get('https://alx-intranet.hbtn.io/status')
-    ty  = type(r.text)
+    ty = type(r.text)
     content = r.text
     print(f"Body response:\n\t- type: {ty}\n\t- content: {content}")
