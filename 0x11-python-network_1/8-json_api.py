09a4ea3e7895d8407110dbd0b48671ea1fdc670d
bakka22 <abobakereltom@gmail.com>
2024-03-09 04:04:40 +0000

    s

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status using requests """
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = http://0.0.0.0:5000/search_user
+    if len(sys.argv) < 2:
+        payload = {'q': ""}
+    else:
+        payload = {'q': sys.argv[1]}
+    r = requests.post(url, data=payload)
+    try:
+        dic = r.json()
+        if dic != {}:
+            id = dic.get('id')
+            name = dic.get('name')
+            print(f"[{id}] {name}")
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
