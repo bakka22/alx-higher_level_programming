4972084d342a40b157c4a3399eb6ae91676544eb
bakka22 <abobakereltom@gmail.com>
2024-03-09 03:21:59 +0000

    s

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ script that fetches https://alx-intranet.hbtn.io/status using requests """
-import requests
-r = requests.get('https://alx-intranet.hbtn.io/status')
-ty  = type(r.text)
-content = r.text
-print(f"Body response:\n\t- type: {ty}\n\t- content: {content}")
+if __name__ == '__main__':
+    import requests
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    ty  = type(r.text)
+    content = r.text
+    print(f"Body response:\n\t- type: {ty}\n\t- content: {content}")
---
+++ 0x11-python-network_1/4-hbtn_status.py~
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status using requests """
+import requests
+r = requests.get('https://alx-intranet.hbtn.io/status')
+ty  = type(r.text)
+content = r.text
+print(f"Body response:\n\t- type: {ty}\n\t- content: {content}")
