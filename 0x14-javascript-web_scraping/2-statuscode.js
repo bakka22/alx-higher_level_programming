8dc78c5b280484cf064a69cda406392ffb64f6b5
bakka22 <abobakereltom@gmail.com>
2024-03-26 23:22:39 +0200

    s

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@ const request = require('request');
 const url = process.argv[2];
 const emp = () => {};
 request(url, function (error, response, body) {
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
   if (error) emp();
 });
