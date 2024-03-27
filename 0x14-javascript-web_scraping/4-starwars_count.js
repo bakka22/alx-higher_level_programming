a7d67b1bfa983556db874cbfa72f81048d4a83bb
bakka22 <abobakereltom@gmail.com>
2024-03-27 02:31:53 +0200

    s

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const emp = () => {};
 const actor = 'https://swapi-api.alx-tools.com/api/people/18/';
 let count = 0;
 request(url, function (error, response, body) {
-  if (error) emp();
+  if (error) throw error;
   const data = JSON.parse(body);
   for (const film of data.results) {
     const charachters = film.characters;
