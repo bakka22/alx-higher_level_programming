f74a84ceb3d590499a1e8b7a28e1a561538a57dc
bakka22 <abobakereltom@gmail.com>
2024-03-27 04:13:23 +0200

    s

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,33 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2] + '/';
+const url = process.argv[2];
 const result = {};
-function makeRequest(j) {
-  return new Promise((resolve, reject) => {
-    request(url + j, function(error, response, body) {
-      if (error) {
-        reject(error);
-      } else {
-        const data = JSON.parse(body);
-        resolve(data.completed ? 1 : 0);
-      }
-    });
-  });
-}
-async function countCompleted(min, max) {
-  let count = 0;
-  for (let j = min; j <= max; j++) {
-    count += await makeRequest(j);
+request(url, function (err, res, body) {
+  if (err) throw err;
+  const all = JSON.parse(body);
+  for (let i = 0; i < all.length; ++i) {
+    task = all[i];
+    if (task.completed && !result[task.userId]) {
+      result[task.userId] = 0;
+    }
+    if (task.completed) ++result[task.userId];
   }
-  return count;
-}
-async function res() {
-  for (let i = 1; i <= 10; ++i) {
-    let count = 0;
-    const min = i * 20 - 19;
-    const max = i * 20;
-    count = await countCompleted(min, max);
-    result[i.toString()] = count;
-  }
-}
-res().then(() => console.log(result));
+  console.log(result);
+});
